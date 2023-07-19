434d5be66fea009a4dd745771ebc14251d8297a6
Mohan5353 <120332828+Mohan5353@users.noreply.github.com>
2023-07-20 01:59:30 +0530

    Added

---
+++ script.js
@@ -0,0 +1,22 @@
+const checkbox = document.getElementById('reg-log');
+const sub_1 = document.getElementById('submit-1');
+checkbox.addEventListener('click', function () {
+    if (checkbox.checked) {
+
+        sub_2 = document.getElementById('submit-2');
+        sub_2.addEventListener('click', function () {
+            var name = document.getElementById('regname').value,
+                email = document.getElementById('regemail').value,
+                password = document.getElementById('regpass').value;
+            if (name != "" && password != "" && email != "") { alert("Data Submitted but Not saved......!!\n.......Signup is in development........"); }
+            else { alert("Please Fill All Fields"); }
+        })
+    }
+});
+sub_1.addEventListener('click', function () {
+    var email = document.getElementById('logemail').value,
+        password = document.getElementById('logpass').value;
+    if (password == "1234" && email == "test@xyzmail.com") {
+        window.location.href = "./Welcome/index.html";
+    }
+});
